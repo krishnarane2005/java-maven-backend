773bc6e689b402542bfed3c3f69eb5464ed58c53
krishnarane2005 <168700563+krishnarane2005@users.noreply.github.com>
2026-01-26 10:53:51 +0530

    Update FestOrgApplicationTests.java

--- src/test/java/com/FestOrg/FestOrg/FestOrgApplicationTests.java
+++ src/test/java/com/FestOrg/FestOrg/FestOrgApplicationTests.java
@@ -3,16 +3,12 @@ package com.FestOrg.FestOrg;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.boot.test.mock.mockito.MockBean;
 
-import com.FestOrg.FestOrg.repository.AdminRepository;
-
-@SpringBootTest
+@SpringBootTest(
+    webEnvironment = SpringBootTest.WebEnvironment.NONE
+)
 @ActiveProfiles("test")
 class FestOrgApplicationTests {
-
-    @MockBean
-    private AdminRepository adminRepository;
 
     @Test
     void contextLoads() {
