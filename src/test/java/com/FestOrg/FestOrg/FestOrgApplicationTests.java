257a4e29bcf8384696fe7705c6aab337257de5e8
krishnarane2005 <168700563+krishnarane2005@users.noreply.github.com>
2026-01-26 10:51:33 +0530

    Add MockBean for AdminRepository in tests

--- src/test/java/com/FestOrg/FestOrg/FestOrgApplicationTests.java
+++ src/test/java/com/FestOrg/FestOrg/FestOrgApplicationTests.java
@@ -3,10 +3,16 @@ package com.FestOrg.FestOrg;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import com.FestOrg.FestOrg.repository.AdminRepository;
 
 @SpringBootTest
 @ActiveProfiles("test")
 class FestOrgApplicationTests {
+
+    @MockBean
+    private AdminRepository adminRepository;
 
     @Test
     void contextLoads() {
